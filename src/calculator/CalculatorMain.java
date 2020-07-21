b60dc7f573d1f48c6a814af6097161c461a68043
Ingibork <ingibork@mail.ru>
2020-07-21 21:55:32 +0300

    21072020

--- src/calculator/CalculatorMain.java
+++ src/calculator/CalculatorMain.java
@@ -1,4 +1,27 @@
 package calculator;
 
-public class CalculatorMain {
+public class CalculatorWithMathExtends extends CalculatorWithOperator{
+
+    public static void main(String[] args) {
+
+        CalculatorWithMathExtends peremennaya = new CalculatorWithMathExtends();
+        double a = (peremennaya.multiply(15,7));
+        double b = (peremennaya.divide(28, 5));
+        double c = (peremennaya.plus(a, 4.1));
+        double e = (peremennaya.exponentiation(b, 2));
+        double result = (peremennaya.plus(c,e));
+        System.out.println(result);
+
+        System.out.println("Результат разделили на 0 = " + result / 0);
+        System.out.println("Результат разделили на 0 = " + result / 0.0d);
+
+
+
+
+ //       4.1 + 15 * 7 + (28 / 5) ^ 2
+
+
+
+
+    }
 }
